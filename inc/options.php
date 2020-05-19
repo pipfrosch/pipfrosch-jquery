2ffba7416c2566f1bf973d8cd89b15092d9a88ca
Alice Wonder <naughty@deviant.email>
2020-05-19 14:49:21 -0700

    tweak

--- inc/options.php
+++ inc/options.php
@@ -4,10 +4,21 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 function pipfrosch_jquery_options_page() {
+  $cdn = get_option( 'pipfrosch_jquery_cdn', false );
+  if ( ! is_bool( $cdn ) ) {
+    $cdn = false;
+  }
+  $parenthesis = '(disabled)';
+  if ( $cdn ) {
+    $parenthesis = '(enabled)';
+  }
+  // right now only one option
+  $cdnhost = 'code.jquery.com';
 ?>
-    <h2>Pipfrosch jQuery Options</h2>
+    <h2>Pipfrosch jQuery Plugin Management</h2>
     <p>jQuery Version: <?php echo PIPJQV; ?></p>
     <p>jQuery Migrate Plugin Version: <?php echo PIPJQMIGRATE; ?></p>
+    <p>Current <abbr title="Content Distribution Network">CDN</abbr>: <?php echo $cdnhost . ' ' . echo $parenthesis;?></p>
     <form method="post" action="options.php">
 <?php
 settings_fields( 'pipfrosch_jquery_options' );
