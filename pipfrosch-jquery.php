e832ddf8ba10a13fdcabe8014e603e66b1f467df
Alice Wonder <naughty@deviant.email>
2020-05-19 14:00:33 -0700

    testing fix

--- pipfrosch-jquery.php
+++ pipfrosch-jquery.php
@@ -28,13 +28,13 @@ well. See the LICENSE file.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 //include_once(ABSPATH . 'wp-includes/pluggable.php');
-include(ABSPATH . "wp-admin/includes/user.php.");
+//include(ABSPATH . "wp-admin/includes/user.php.");
 
 define( "PIPFROSCH_JQUERY_PLUGIN_DIR", plugin_dir_path( __FILE__ ) );
 
 require_once( PIPFROSCH_JQUERY_PLUGIN_DIR . 'versions.php' );
 require_once( PIPFROSCH_JQUERY_PLUGIN_DIR . 'inc/functions.php' );
-require_once( PIPFROSCH_JQUERY_PLUGIN_DIR . 'inc/options.php' );
+//require_once( PIPFROSCH_JQUERY_PLUGIN_DIR . 'inc/options.php' );
 
 register_activation_hook( __FILE__, 'pipfrosch_jquery_set_expires_header' );
 
