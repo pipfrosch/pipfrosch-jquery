84e8dce8b9035ae29a7bea19839cf83384755890
Alice Wonder <naughty@deviant.email>
2020-05-20 02:21:38 -0700

    trying something...

--- inc/options.php
+++ inc/options.php
@@ -4,32 +4,27 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 function pipfrosch_jquery_options_page() {
-  $cdn = get_option( 'pipfrosch_jquery_cdn', false );
-  if ( ! is_bool( $cdn ) ) {
-    $cdn = false;
-  }
+  $cdn = pipfrosch_jquery_getas_boolean( 'pipfrosch_jquery_cdn', false );
   $parenthesis = '(disabled)';
   if ( $cdn ) {
     $parenthesis = '(enabled)';
   }
-  // right now only one option
-  //$cdnhost = 'code.jquery.com';
-  $cdnhost = pipfrosch_press_sanitize_cdnhost( get_option( 'pipfrosch_jquery_cdnhost' ) );
+  $cdnhost = pipfrosch_jquery_getstring_cdnhost();
   $s = array( '/CDN$/' , '/CDNJS/' );
   $r = array( '<abbr>CDN</abbr>' , '<abbr>CDNJS</abbr>' );
   $cdnhost = preg_replace($s, $r, $cdnhost);
-?>
-    <h2>Pipfrosch jQuery Plugin Management</h2>
-    <p>jQuery Version: <?php echo PIPJQV; ?></p>
-    <p>jQuery Migrate Plugin Version: <?php echo PIPJQMIGRATE; ?></p>
-    <p>Current <abbr title="Content Distribution Network">CDN</abbr>: <?php echo $cdnhost . ' ' . $parenthesis;?></p>
-    <form method="post" action="options.php">
-<?php
-settings_fields( 'pipfrosch_jquery_options' );
-do_settings_sections( 'pipfrosch_jquery_options' );
-?>
-      <p>Note that the ‘Use Subresource Integrity’ option only has meaning when ‘Use Content Distribution Network’ is enabled.</p>
-      <?php  submit_button(); ?>
-    </form>
-<?php
+  $html  = '    <h2>Pipfrosch jQuery Plugin Management</h2>' . PHP_EOL;
+  $html .= '    <p>jQuery Version: ' . PIPJQV . '</p>' . PHP_EOL;
+  $html .= '    <p>jQuery Migrate Plugin Version: ' . PIPJQMIGRATE . '</p>' . PHP_EOL;
+  $html .= '    <p>Current <abbr title="Content Distribution Network">CDN</abbr>: ' . $cdnhost . ' ' . $parenthesis . '</p>' . PHP_EOL;
+  $html .= '    <form method="post" action="options.php">' . PHP_EOL;
+  echo $html;
+  settings_fields( 'pipfrosch_jquery_options' );
+  do_settings_sections( 'pipfrosch_jquery_options' );
+  $html  = '      <p>Note that the ‘Use Subresource Integrity’ option only has meaning when ‘Use Content Distribution Network’ is enabled.</p>' . PHP_EOL;
+  $html .= get_submit_button();
+  $html .= '    </form>';
+  echo $html;
 }
+
+
