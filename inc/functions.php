67d9289650e60ad6a89864255cff28ed0499e9a7
Alice Wonder <naughty@deviant.email>
2020-05-20 17:19:24 -0700

    finished phpdoc headers

--- inc/functions.php
+++ inc/functions.php
@@ -99,7 +99,21 @@ function pipjq_get_cdnhost_option() {
 }
 
 // the callback to sanitize checkbox string
-function pipjq_sanitize_checkbox( $input ) {
+
+/**
+ * Sanitize checkbox input.
+ *
+ * This plugin likes the faux boolean options set to be a string of "0" for false
+ *  and "1" for true. The form sets a value a "1" when checked. If a string that
+ *  evaluates as the integer 1 when recast to integer is supplies, this function
+ *  will output the string "1". Any other value and it outputs the string "0".
+ *
+ * @param string The string passed to this callback from the WordPress options form
+ *               processing.
+ *
+ * @return string
+ */
+function pipjq_sanitize_checkbox( string $input ) {
   $input = sanitize_text_field( $input );
   if ( is_numeric( $input ) ) {
     $num = intval( $input );
@@ -111,6 +125,16 @@ function pipjq_sanitize_checkbox( $input ) {
 }
 
 /* initiate options */
+
+/**
+ * Initialize options
+ *
+ * This function makes sure the options are defined in the WordPress options
+ *  database and sets them to the default values if they are not already
+ *  defined. This script is run during plugin activation.
+ *
+ * @return void
+ */
 function pipjq_initialize_options() {
   $foo = pipjq_get_option_as_boolean( 'pipjq_migrate' );
   $foo = pipjq_get_option_as_boolean( 'pipjq_cdn', false );
@@ -118,7 +142,21 @@ function pipjq_initialize_options() {
   $foo = pipjq_get_cdnhost_option();
 }
 
-/* provide fallback if jQuery does not load from CDN */
+/**
+ * Fallback for a CDN failure.
+ *
+ * In the event that a client can not retrieve the needed jQuery file from a CDN or
+ *  it retrieves the file but the SRI does not match, a website will be broken without
+ *  a fallback. This function provides a small HTML snippet for a script that verifies
+ *  retrieval of the jQuery library or the jQuery Migrate plugin was successful, and
+ *  instructs the client to download the copy served from the local website if it was
+ *  not.
+ *
+ * @param bool When true, the HTML snippet is for testing the main jQuery library. When
+ *             false, it is for testing the Migrate plugin.
+ *
+ * @return string
+ */
 function pipjq_fallback_for_cdn_failure( bool $core = true ) {
   $html = '<script>' . PHP_EOL . '  // Fallback to load locally if CDN fails' . PHP_EOL;
   if ($core) {
@@ -132,10 +170,30 @@ function pipjq_fallback_for_cdn_failure( bool $core = true ) {
   return $html;
 }
 
-/* The following two functions are only used with a CDN.
-   The first is used if SRI is enabled (recommended), the second
-   if SRI is disabled */
-function pipjq_add_sri_attributes( $tag, $handle, $source ) {
+/**
+ * Add the SRI and CrossOrigin attributes
+ *
+ * The WordPress core function `wp_register_script()` does not provide a means
+ *  for adding attributes to a script node. When a script is served from a third
+ *  party resource, it really needs to have both an `integrity` and a
+ *  `crossorigin` attribute set so the browser can validate both the integrity of
+ *  the resource being downloaded from the third party, and what authentication
+ *  should be performed (always anonymous for a public CDN, which at least with
+ *  some browsers means cookies are not set or sent even if they exist. Yay for
+ *  privacy). This function is used as a callback in a the WordPress
+ *  `script_loader_tag` to rewrite the <script></script> node with the appropriate
+ *  attributes for both SRI and CrossOrigin, along with the fallback HTML snippet
+ *  from the `pipjq_fallback_for_cdn_failure()` function.
+ *
+ * @param string The original version of the script tag.
+ * @param string The handle that was used to register the script associated with
+ *               the tag in the first parameter.
+ * @param string The contents of the `src` attribute in the tag from the first
+ *               parameter.
+ *
+ * @return string
+ */
+function pipjq_add_sri_attributes( string $tag, string $handle, string $source ) {
   switch( $handle ) {
     case 'jquery-core':
       $html = pipjq_fallback_for_cdn_failure();
@@ -147,7 +205,25 @@ function pipjq_add_sri_attributes( $tag, $handle, $source ) {
   }
   return $tag;
 }
-function pipjq_add_crossorigin_attribute( $tag, $handle, $source ) {
+
+/**
+ * Add CrossOrigin attribute
+ *
+ * There are a valid reasons why a webmaster may not want this WordPress plugin
+ *  adding the SRI tag. For example, they may have a different plugin that already
+ *  does that from a database. In such cases, the CrossOrigin attribute should
+ *  still be added, along with the fallback HTML snippet. This function does that.
+ *  See the PHPdoc header for pipjq_add_sri_attributes().
+ *
+ * @param string The original version of the script tag.
+ * @param string The handle that was used to register the script associated with
+ *               the tag in the first parameter.
+ * @param string The contents of the `src` attribute in the tag from the first
+ *               parameter.
+ *
+ * @return string
+ */
+function pipjq_add_crossorigin_attribute( string $tag, string $handle, string $source ) {
   switch( $handle ) {
     case 'jquery-core':
       $html = pipjq_fallback_for_cdn_failure();
@@ -161,6 +237,18 @@ function pipjq_add_crossorigin_attribute( $tag, $handle, $source ) {
   return '<script src="' . $source . '" crossorigin="anonymous"></script>' . PHP_EOL . $html;
 }
 
+/**
+ * Generate src attribute for the script nodes.
+ *
+ * This function creates the appropriate `src` attribute needed to register the
+ *  jQuery and Migrate scripts with WordPress based upon the CDN choice. It returns
+ *  an object with those strings as properties, and also a boolean property that
+ *  specifies whether or not the `src` attributes are for a CDN.
+ *
+ * @param string The name of the CDN host.
+ *
+ * @return stdClass
+ */
 function pipjq_script_src( string $cdnhost="localhost" ) {
   $rs = new stdClass();
   switch( $cdnhost ) {
@@ -198,7 +286,17 @@ function pipjq_script_src( string $cdnhost="localhost" ) {
   return $rs;
 }
 
-// defines the included jQuery to be served
+/**
+ * Update WordPress Core JavaScript
+ *
+ * This is the heart of this WordPress plugin. This function will deregister the
+ *  versions of jQuery and jQuery Migrate that ship with WordPress Core and
+ *  register the more modern versions that ship with this plugin, either locally
+ *  or from a CDN, with the appropriate attributes and fallback HTML when served
+ *  from a CDN.
+ *
+ * @return void
+ */
 function pipjq_update_wpcore_jquery() {
   //get the settings and validate
   $migrate = pipjq_get_option_as_boolean( 'pipjq_migrate' );
@@ -225,7 +323,19 @@ function pipjq_update_wpcore_jquery() {
   }
 }
 
-// initiated options and creates the .htaccess file. I do not like including a .htaccess within a plugin zip archive.
+/**
+ * Plugin activation script.
+ *
+ * When activating the plugin this function is run. It will initialize the options
+ *  this plugin uses to their default values (unless already set) and if the server
+ *  has write permission to the directory for this plugin *and* a `.htaccess` file
+ *  does not already exist in it, it will create a `.htaccess` file that is
+ *  compatible with the Apache 2 `mod_expires.c` module so that jQuery and the
+ *  Migrate plugin are served with a header telling the client it can cache those
+ *  files for up to a year.
+ *
+ * @return void
+ */
 function pipjq_activation() {
   pipjq_initialize_options();
   $htaccess = PIPJQ_PLUGIN_DIR . ".htaccess";
@@ -245,16 +355,33 @@ function pipjq_activation() {
   return;
 }
 
-//settings
+/* functions specific to the WordPress Settings API */
 
 // the callback for add_settings_section
+
+/**
+ * Settings form helpers
+ *
+ * Callback function used by the WordPress core function `add_settings_section()`
+ *  to provide some recommendations for the plugin settings.
+ *
+ * @TODO These strings need to be internationalized. When internationalizing, the
+ *       contents between the left and right curly quotes need to MATCH the strings
+ *       used as labels in the corresponding `add_settings_field()` calls.
+ *
+ * @return void
+ */
 function pipjq_settings_form_text_helpers() {
-  echo ('<p>It is recommended that you enable the ‘Use Migrate Plugin’ option (default).</p>');
-  echo ('<p>It is recommended that you enable the ‘Use Content Distribution Network’ option.</p>');
-  echo ('<p>It is recommended that you enable the ‘Use Subresource Integrity’ option (default).</p>');
+  echo ('<p>It is recommended that you enable the ‘<em>Use Migrate Plugin</em>’ option (default).</p>');
+  echo ('<p>It is recommended that you enable the ‘<em>Use Content Distribution Network</em>’ option.</p>');
+  echo ('<p>It is recommended that you enable the ‘<em>Use Subresource Integrity</em>’ option (default).</p>');
 }
 
-// input tag for migrate
+/**
+ * Generate checkbox input HTML snippet for the ‘Use Migrate Plugin’ option.
+ *
+ * @return void
+ */
 function pipjq_migrate_input_tag() {
   $migrate = pipjq_get_option_as_boolean( 'pipjq_migrate' );
   $checked = '';
@@ -264,7 +391,11 @@ function pipjq_migrate_input_tag() {
   echo '<input type="checkbox" name="pipjq_migrate" id="pipjq_migrate" value="1"' . $checked . '>';
 }
 
-// input tag for use cdn
+/**
+ * Generate checkbox input HTML snippet for the ‘Use Content Distribution Network’ option.
+ *
+ * @return void
+ */
 function pipjq_cdn_input_tag() {
   $cdn = pipjq_get_option_as_boolean( 'pipjq_cdn', false );
   $checked = '';
@@ -274,9 +405,28 @@ function pipjq_cdn_input_tag() {
   echo '<input type="checkbox" name="pipjq_cdn" id="pipjq_cdn" value="1"' . $checked . '>';
 }
 
-// select tag for cdnhost
+/**
+ * Generate checkbox input HTML snippet for the ‘Use Subresource Integrity’ option.
+ *
+ * @return void
+ */
+function pipjq_sri_input_tag() {
+  $sri = pipjq_get_option_as_boolean( 'pipjq_sri' );
+  $checked = '';
+  if ($sri) {
+    $checked = ' checked="checked"';
+  }
+  echo '<input type="checkbox" name="pipjq_sri" id="pipjq_sri" value="1"' . $checked . '>';
+}
+
+/**
+ * Generate select and child option tag HTML snippet for the ‘Select Public CDN Service’ menu.
+ *
+ * @return void
+ */
 function pipjq_cdnhost_select_tag() {
   $cdnhost = pipjq_get_cdnhost_option();
+  // translators: This array is of proper names and they do not get translated
   $values = array('jQuery.com CDN',
                   'CloudFlare CDNJS',
                   'jsDelivr CDN',
@@ -294,16 +444,20 @@ function pipjq_cdnhost_select_tag() {
   echo $html;
 }
 
-// input tag for SRI
-function pipjq_sri_input_tag() {
-  $sri = pipjq_get_option_as_boolean( 'pipjq_sri' );
-  $checked = '';
-  if ($sri) {
-    $checked = ' checked="checked"';
-  }
-  echo '<input type="checkbox" name="pipjq_sri" id="pipjq_sri" value="1"' . $checked . '>';
-}
-
+/**
+ * Creates the HTML needed for the Settings API form.
+ *
+ * This function notifies the administrator what version of the core jQuery library and
+ *  jQuery Migrate plugin are made available, as well as what Content Distribution Network
+ *  is currently configured to be used. It then creates the HTML <form/> node that an
+ *  administrator can use to change the settings associated with this WordPress plugin.
+ *  This function is called as a callback by the WordPress core function `add_options_page()`
+ *  to make the form available in the Settings menu.
+ *
+ * @TODO Internationalization of the strings is needed.
+ *
+ * @return void
+ */
 function pipjq_options_page_form() {
   $cdn = pipjq_get_option_as_boolean( 'pipjq_cdn', false );
   $parenthesis = '(disabled)';
@@ -328,6 +482,17 @@ function pipjq_options_page_form() {
   echo $html;
 }
 
+/**
+ * Set up the WordPress Settings API.
+ *
+ * This function is a callback added to the `admin_init` action by the WordPress
+ *  Core function `add_action()` function used in the main plugin PHP script.
+ *
+ * @TODO The second arguments of the WordPress Core function `add_settings_field()`
+ *       calls used within this function need to be internationalized.
+ *
+ * @return void
+ */
 function pipjq_register_settings() {
   register_setting( PIPJQ_OPTIONS_GROUP,
                     'pipjq_migrate',
@@ -376,6 +541,17 @@ function pipjq_register_settings() {
                       array( 'label_for' => 'pipjq_cdnhost' ) );
 }
 
+/**
+ * Registers the Settings API form for changing options.
+ *
+ * Basically just wraps the WordPress Core function `add_options_page()`
+ *  in a callback added to the `admin_menu` action by the WordPress
+ *  Core function `add_action()` function used in the main plugin PHP script.
+ *
+ * @TODO The word 'Options' in the first and second arguments should be internationalized.
+ *
+ * @return void
+ */
 function pipjq_register_options_page() {
   add_options_page( 'jQuery ' . PIPJQV . ' Options',
                     'jQuery Options',
--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit; }
 
-# remove options
+# remove options set by the plugin
 delete_option( 'pipjq_migrate' );
 delete_option( 'pipjq_cdn' );
 delete_option( 'pipjq_sri' );
