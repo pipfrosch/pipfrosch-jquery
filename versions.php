3b860e6517d4210346b869059b80f81c249b7db3
pipfrosch <pipfroschpress@gmail.com>
2020-05-21 19:30:43 -0700

    add cdnjs SRI for migrate plugin

--- versions.php
+++ versions.php
@@ -8,4 +8,6 @@ define( "PIPJQV", "3.5.1" );
 define( "PIPJQVSRI", "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" );
 define( "PIPJQMIGRATE", "3.3.0" );
 define( "PIPJQMIGRATESRI", "sha256-wZ3vNXakH9k4P00fNGAlbN0PkpKSyhRa76IFy4V1PYE=" );
-
+//  for migrate plugij
+//   CDNJS has a //# sourceMappingURL=jquery-migrate.min.map at the end of the file
+define( "PIPJQMIGRATESRI_CDNJS", "sha256-lubBd1CVmtB9FHh+c1CWkr4jCSiszGj7bhzWPpNgw1A=" );
